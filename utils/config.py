313ba0ac0f8474886451fb595ed2faf3326c7e8f
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-02-08 08:05:03 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,15 +1,15 @@
 token = ""
-prefix = "-"
-ltc_addy ="LeLGGkBm3MsEqCnHfVfGcaB5oqR3ZkTcNp"
+prefix = "!"
+ltc_addy ="LiN9QN6kZpP5dB1A2v3ggVboWz6CHuhmVy"
 api_key = "t-66909f3ece6f17001cc3e1da-bea57bac36494f5ba91444f1"
 pvt_key = ""
-channels =[1261396891552317472,1261183439080521870]
+channels =[1319492046062620792]
 message = """
 
 """
 dm_msg = {
-        "addy": "LeLGGkBm3MsEqCnHfVfGcaB5oqR3ZkTcNp",
+        "addy": "LiN9QN6kZpP5dB1A2v3ggVboWz6CHuhmVy",
 }
 
-adress_to_check =["LeLGGkBm3MsEqCnHfVfGcaB5oqR3ZkTcNp"]
+adress_to_check =["LiN9QN6kZpP5dB1A2v3ggVboWz6CHuhmVy"]
 gemini_key =""
